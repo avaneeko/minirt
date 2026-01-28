d727af5fbc49aca07ec0442c7f1544f0a4e3fae3
avaneeko <222704844+avaneeko@users.noreply.github.com>
2026-01-28 16:15:50 +0200

    add: Prototype SphereIntersectionDescriptor. add: hit struct.

---
+++ src/sphere_intersection.h
@@ -0,0 +1,11 @@
+#ifndef SPHERE_INTERSECTION_H
+# define SPHERE_INTERSECTION_H
+
+/**
+ * Sphere intersection functions.
+ */
+
+int
+intersect_spheres();
+
+#endif
---
+++ src/sphere_intersection_desc.h
@@ -0,0 +1,26 @@
+#ifndef SPHERE_INTERSECTION_DESC_H
+# define SPHERE_INTERSECTION_DESC_H
+
+/**
+ * Sphere intersection descriptor.
+ *
+ * This describes all the information needed to calculate the closest sphere
+ * intersection.
+ */
+
+# include "sphere_def.h"
+# include "hit_def.h"
+# include "ray_def.h"
+
+struct s_sphere_intersection_desc
+{
+	t_sphere const	*spheres;
+	t_u32			sphere_len;
+	t_ray			ray;
+    t_f32           dist_min;
+    t_f32           dist_max;
+	t_hit			*hit;
+};
+typedef struct s_sphere_intersection_desc t_sphere_intersection_desc;
+
+#endif
