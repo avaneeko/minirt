b2eeaa15dd936a9f0da9ad20d05c4f991af2c73e
avaneeko <222704844+avaneeko@users.noreply.github.com>
2026-02-10 13:36:56 +0200

    add: Add ground plane to the debug world.

--- src/dbg_wrld.c
+++ src/dbg_wrld.c
@@ -33,7 +33,7 @@ InitSpheres(t_sphere **spheres, t_u32 *len, t_u32 *cap)
 		_Exit(-1);
 	}
 	// spheres[0]->pos = (t_v3){0.f,0.f,20.6f};
-	(*spheres)[0].pos = (t_v3){0.f,15.f,0.f};
+	(*spheres)[0].pos = (t_v3){0.f,45.f,0.f};
 	(*spheres)[0].r = 12.6f / 2.f;
 	(*spheres)[0].col = (t_v3){10 / 255.f, 0 / 255.f, 255 / 255.f};
 
@@ -49,7 +49,7 @@ InitSpheres(t_sphere **spheres, t_u32 *len, t_u32 *cap)
 	(*spheres)[3].r = 12.6f / 4.f;
 	(*spheres)[3].col = (t_v3){60.f / 255.f, 60.f / 255.f, 0 / 255.f};
 
-	*len = 4;
+	*len = 1;
 	*cap = 16;
 }
 
@@ -90,10 +90,10 @@ InitPlanes(t_plane **planes, t_u32 *len, t_u32 *cap)
 	}
 
 	(*planes)[0].pos = (t_v3){0.0f, 0.0f, -10.0f};
-	(*planes)[0].ang = (t_v3){0.0f, 1.0f, 0.0f};
-	(*planes)[0].col = (t_v3){0.f, 0.f, 1.f};
+	(*planes)[0].ang = (t_v3){0.0f, 0.0f, 1.0f};
+	(*planes)[0].col = (t_v3){0.3f, 0.3f, 0.3f};
 
-	*len = 0;
+	*len = 1;
 	*cap = 16;
 }
 
@@ -104,7 +104,7 @@ InitLights(t_light *l, t_ambient *a)
 	l->bright = 1.f;
 	l->col = (t_v3){1.f, 1.f, 1.f};
 
-	a->bright = .1f;
+	a->bright = .3f;
 	a->col = (t_v3){1.f, 1.f, 1.f};
 }
 
