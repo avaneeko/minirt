511b1ba84826c71e8b9b334cd781dfe4ff9494ea
avaneeko <222704844+avaneeko@users.noreply.github.com>
2026-02-12 16:45:14 +0200

    norm: Normify src/sphere_intersection.c

--- src/sphere_intersection.c
+++ src/sphere_intersection.c
@@ -1,39 +1,58 @@
-#include <iso646.h>
-#include <math.h>
+#include "minitypes.h"
+#include "v3.h"
 #include "sphere_intersection.h"
+#include <math.h>
 
+void	fill_hit(t_sphere_intersection_desc const *desc,
+			t_f32 closest, t_u32 best_idx)
+{
+	desc->hit->dist = closest;
+	desc->hit->col = desc->spheres[best_idx].col;
+	v3_muladds(&desc->ray.pos, &desc->ray.dir, closest, &desc->hit->pos);
+	v3_sub(&desc->hit->pos, &desc->spheres[best_idx].pos, &desc->hit->norm);
+	v3_scalar_div(&desc->hit->norm, desc->spheres[best_idx].r,
+		&desc->hit->norm);
+	desc->hit->front = (v3_dot(&desc->ray.dir, &desc->hit->norm) < 0.0f);
+	if (!desc->hit->front)
+		v3_scalar_mul(&desc->hit->norm, -1.0f, &desc->hit->norm);
+}
+
+/*
+dist[0] is dist_min
+dist[1] is dist_max
+n[0] is a
+n[1] is b
+n[2] is c
+n[3] is disc
+n[4] is sqrtd
+n[5] is d0
+n[6] is d1
+*/
 int	sphere_intersect_dist(t_sphere const *sp, t_ray const *r,
-							t_f32 dist_min, t_f32 dist_max,
+							t_f32 dist[2],
 							t_f32 *out_dist)
 {
 	t_v3	oc;
-	t_f32	a, b, c;
-	t_f32	disc, sqrtd;
-	t_f32	d0, d1;
+	t_f32	n[7];
 
-	v3_sub(&sp->pos, &r->pos, &oc); /* oc = C - O */
-
-	a = v3_dot(&r->dir, &r->dir);
-	b = -2.0f * v3_dot(&r->dir, &oc);
-	c = v3_dot(&oc, &oc) - sp->r * sp->r;
-
-	disc = b * b - 4.0f * a * c;
-	if (disc < 0.0f)
-		return 0;
-
-	sqrtd = sqrtf(disc);
-	d0 = (-b - sqrtd) / (2.0f * a);
-	d1 = (-b + sqrtd) / (2.0f * a);
-
-	if (d0 >= dist_min && d0 <= dist_max)
-		return (*out_dist = d0, 1);
-	if (d1 >= dist_min && d1 <= dist_max)
-		return (*out_dist = d1, 1);
-	return 0;
+	v3_sub(&sp->pos, &r->pos, &oc);
+	n[0] = v3_dot(&r->dir, &r->dir);
+	n[1] = -2.0f * v3_dot(&r->dir, &oc);
+	n[2] = v3_dot(&oc, &oc) - sp->r * sp->r;
+	n[3] = n[1] * n[1] - 4.0f * n[0] * n[2];
+	if (n[3] < 0.0f)
+		return (0);
+	n[4] = sqrtf(n[3]);
+	n[5] = (-n[1] - n[4]) / (2.0f * n[0]);
+	n[6] = (-n[1] + n[4]) / (2.0f * n[0]);
+	if (n[5] >= dist[0] && n[5] <= dist[1])
+		return (*out_dist = n[5], 1);
+	if (n[6] >= dist[0] && n[6] <= dist[1])
+		return (*out_dist = n[6], 1);
+	return (0);
 }
 
-void
-intersect_spheres(t_sphere_intersection_desc const *desc)
+void	intersect_spheres(t_sphere_intersection_desc const *desc)
 {
 	t_f32			closest;
 	t_u32			best_idx;
@@ -46,31 +65,14 @@ intersect_spheres(t_sphere_intersection_desc const *desc)
 	while (i < desc->sphere_len)
 	{
 		if (sphere_intersect_dist(desc->spheres + i, &desc->ray,
-									desc->dist_min, closest, &d))
+				(float []){desc->dist_min, closest}, &d))
 		{
 			closest = d;
 			best_idx = i;
 		}
 		i++;
 	}
-
 	if (best_idx == ~(t_u32)0)
 		return ;
-
-	desc->hit->dist = closest; /* before or after early dist reject? */
-
-	/* material/color */
-	desc->hit->col = desc->spheres[best_idx].col;
-
-	/* pos = O + D*dist */
-	v3_muladds(&desc->ray.pos, &desc->ray.dir, closest, &desc->hit->pos);
-
-	/* norm = normalize((pos - C) / r) */
-	v3_sub(&desc->hit->pos, &desc->spheres[best_idx].pos, &desc->hit->norm);
-	v3_scalar_div(&desc->hit->norm, desc->spheres[best_idx].r, &desc->hit->norm);
-
-	/* faceforward */
-	desc->hit->front = (v3_dot(&desc->ray.dir, &desc->hit->norm) < 0.0f);
-	if (!desc->hit->front)
-		v3_scalar_mul(&desc->hit->norm, -1.0f, &desc->hit->norm);
+	fill_hit(desc, closest, best_idx);
 }
