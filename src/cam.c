e7a955ea0309ed6bfd3e8e26263820d160337768
avaneeko <222704844+avaneeko@users.noreply.github.com>
2026-02-12 16:43:19 +0200

    norm: Normify src/cam.c

--- src/cam.c
+++ src/cam.c
@@ -14,17 +14,15 @@
 static
 t_v3	choose_up(t_cam *cam)
 {
-	t_v3 up;
+	t_v3	up;
 
 	up = (t_v3){0.0f, 0.0f, 1.0f};
 	if (fabsf(v3_dot(&cam->fwd, &up)) < 0.999f)
-		return up;
-
+		return (up);
 	up = (t_v3){0.0f, 1.0f, 0.0f};
 	if (fabsf(v3_dot(&cam->fwd, &up)) < 0.999f)
-		return up;
-
-	return (t_v3){1.0f, 0.0f, 0.0f};
+		return (up);
+	return ((t_v3){1.0f, 0.0f, 0.0f});
 }
 
 int	cam_init(t_cam *cam)
@@ -34,11 +32,9 @@ int	cam_init(t_cam *cam)
 	v3_cross(&cam->fwd, &up_hint, &cam->right);
 	if (!v3_normalize_safe(&cam->right))
 		return (0);
-
 	v3_cross(&cam->right, &cam->fwd, &cam->up);
-    cam->aspect = (t_f32)WINDOW_WIDTH / (t_f32)WINDOW_HEIGHT;
-    cam->half_h = tanf(cam->fov * 0.5f);
-    cam->half_w = cam->aspect * cam->half_h;
-
+	cam->aspect = (t_f32)WINDOW_WIDTH / (t_f32)WINDOW_HEIGHT;
+	cam->half_h = tanf(cam->fov * 0.5f);
+	cam->half_w = cam->aspect * cam->half_h;
 	return (v3_normalize_safe(&cam->up));
 }
