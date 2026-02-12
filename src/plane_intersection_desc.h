8afda4e3a237c415e0f412549ac5b64e6e7cc9e6
avaneeko <222704844+avaneeko@users.noreply.github.com>
2026-02-12 16:45:12 +0200

    norm: Normify src/plane_intersection_desc.h

--- src/plane_intersection_desc.h
+++ src/plane_intersection_desc.h
@@ -21,6 +21,6 @@ struct s_plane_intersection_desc
 	t_f32			dist_max;
 	t_hit			*hit;
 };
-typedef struct s_plane_intersection_desc t_plane_intersection_desc;
+typedef struct s_plane_intersection_desc	t_plane_intersection_desc;
 
 #endif
