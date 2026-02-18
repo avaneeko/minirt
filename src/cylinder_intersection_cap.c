49b649ec2b76445c232c715763591ab21b9235ba
avaneeko <222704844+avaneeko@users.noreply.github.com>
2026-02-18 15:18:34 +0200

    norm: Normify cylinder_intersection_cap.c

--- src/cylinder_intersection_cap.c
+++ src/cylinder_intersection_cap.c
@@ -6,7 +6,7 @@
 /*   By: losypenk <losypenk@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 14:23:56 by losypenk          #+#    #+#             */
-/*   Updated: 2026/02/16 14:23:56 by losypenk         ###   ########.fr       */
+/*   Updated: 2026/02/18 15:18:05 by losypenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,102 @@
  */
 
 #include "cylinder_intersection_desc.h"
+#include "inter_cyl_cap_ctx_def.h"
+#include "minitypes.h"
+#include "utils.h"
 #include "v3.h"
 #include <math.h>
 
+/*
+t is f[0]
+k is f[1]
+*/
 static
-int	ray_intersect_cylinder_cap(t_cylinder const *cy, t_v3 cap_center,
-									t_v3 cap_norm, t_ray const *r,
-									t_f32 dist_min, t_f32 dist_max,
-									t_f32 *out_dist)
+int	ray_intersect_cylinder_cap(t_inter_cyl_cap_ctx const *ctx)
 {
-    t_f32 denom = v3_dot(&cap_norm, &r->dir);
-    if (fabsf(denom) < 1e-6f)
-        return 0;
+	t_f32 const	denom = v3_dot(&ctx->cap_norm, &ctx->r->dir);
+	t_v3		oc;
+	t_v3		p;
+	t_f32		f[2];
+	t_v3		v;
 
-    t_v3 oc;
-    v3_sub(&cap_center, &r->pos, &oc);
+	if (fabsf(denom) < 1e-6f)
+		return (0);
+	v3_sub(&ctx->cap_center, &ctx->r->pos, &oc);
+	f[0] = v3_dot(&ctx->cap_norm, &oc) / denom;
+	if (f[0] < ctx->dist_min || f[0] > ctx->dist_max)
+		return (0);
+	v3_muladds(&ctx->r->pos, &ctx->r->dir, f[0], &p);
+	v3_sub(&p, &ctx->cap_center, &v);
+	f[1] = v3_dot(&v, &ctx->cy->ang);
+	v.x -= ctx->cy->ang.x * f[1];
+	v.y -= ctx->cy->ang.y * f[1];
+	v.z -= ctx->cy->ang.z * f[1];
+	if (v3_dot(&v, &v) > ctx->cy->r * ctx->cy->r)
+		return (0);
+	*ctx->out_dist = f[0];
+	return (1);
+}
 
-    t_f32 t = v3_dot(&cap_norm, &oc) / denom;
-    if (t < dist_min || t > dist_max)
-        return 0;
+static
+void	fill_hit(t_cylinder_intersection_desc const *desc,
+			t_u32 best_idx, t_f32 closest, t_u8 is_bot)
+{
+	if (best_idx == ~(t_u32)0)
+		return ;
+	desc->hit->dist = closest;
+	desc->hit->col = desc->cylinders[best_idx].col;
+	v3_muladds(&desc->ray.pos, &desc->ray.dir, closest, &desc->hit->pos);
+	if (!is_bot)
+		desc->hit->norm = desc->cylinders[best_idx].ang;
+	else
+		desc->hit->norm = (t_v3){-desc->cylinders[best_idx].ang.x,
+									-desc->cylinders[best_idx].ang.y,
+									-desc->cylinders[best_idx].ang.z};
+	desc->hit->front = (v3_dot(&desc->ray.dir, &desc->hit->norm) < 0.0f);
+	if (!desc->hit->front)
+		v3_scalar_mul(&desc->hit->norm, -1.0f, &desc->hit->norm);
+}
 
-    /* p = O + D*t */
-    t_v3 p;
-    v3_muladds(&r->pos, &r->dir, t, &p);
+static
+int	trace_caps(t_inter_cyl_cap_ctx *ctx, t_u8 *is_bot)
+{
+	t_v3 top;
 
-    /* radial check: distance from axis <= r */
-    t_v3 v;
-    v3_sub(&p, &cap_center, &v);
+	top = ctx->cy->pos;
+	top.x += ctx->cy->ang.x * ctx->cy->h;
+	top.y += ctx->cy->ang.y * ctx->cy->h;
+	top.z += ctx->cy->ang.z * ctx->cy->h;
 
-    t_f32 k = v3_dot(&v, &cy->ang);
-    v.x -= cy->ang.x * k;
-    v.y -= cy->ang.y * k;
-    v.z -= cy->ang.z * k;
+	mcpy(&ctx->cap_center, &top, sizeof(top));
+	if (ray_intersect_cylinder_cap(ctx))
+	{
+		*is_bot = 0;
+		return (1);
+	}
+	return (0);
+}
 
-    if (v3_dot(&v, &v) > cy->r * cy->r)
-        return 0;
+static
+int	trace_caps2(t_inter_cyl_cap_ctx *ctx, t_u8 *is_bot)
+{
+	t_v3 bot;
 
-    *out_dist = t;
-    return 1;
+	bot = ctx->cy->pos;
+	bot.x -= ctx->cy->ang.x * ctx->cy->h;
+	bot.y -= ctx->cy->ang.y * ctx->cy->h;
+	bot.z -= ctx->cy->ang.z * ctx->cy->h;
+
+	mcpy(&ctx->cap_center, &bot, sizeof(bot));
+	ctx->cap_norm.x = -ctx->cap_norm.x;
+	ctx->cap_norm.y = -ctx->cap_norm.y;
+	ctx->cap_norm.z = -ctx->cap_norm.z;
+	if (ray_intersect_cylinder_cap(ctx))
+	{
+		*is_bot = 1;
+		return (1);
+	}
+	return (0);
 }
 
 void	intersect_cylinder_cap(t_cylinder_intersection_desc const *desc)
@@ -65,54 +123,19 @@ void	intersect_cylinder_cap(t_cylinder_intersection_desc const *desc)
 
 	closest = desc->hit->dist;
 	best_idx = ~(t_u32)0;
-
-	i = 0;
-	while (i < desc->cylinder_len)
+	i = ~0;
+	while (++i < desc->cylinder_len)
 	{
-		t_v3 top = desc->cylinders[i].pos;
-		t_v3 bot = desc->cylinders[i].pos;
-
-		top.x += desc->cylinders[i].ang.x * desc->cylinders[i].h;
-		top.y += desc->cylinders[i].ang.y * desc->cylinders[i].h;
-		top.z += desc->cylinders[i].ang.z * desc->cylinders[i].h;
-
-		bot.x -= desc->cylinders[i].ang.x * desc->cylinders[i].h;
-		bot.y -= desc->cylinders[i].ang.y * desc->cylinders[i].h;
-		bot.z -= desc->cylinders[i].ang.z * desc->cylinders[i].h;
-
-		if (ray_intersect_cylinder_cap(desc->cylinders + i, top,
-									desc->cylinders[i].ang, &desc->ray, desc->dist_min, closest, &d))
+		if (trace_caps(&(t_inter_cyl_cap_ctx)
+			{desc->cylinders + i, desc->cylinders[i].pos,
+			desc->cylinders[i].ang, &desc->ray, desc->dist_min, closest, &d}, &is_bot)
+			|| trace_caps2(&(t_inter_cyl_cap_ctx)
+			{desc->cylinders + i, desc->cylinders[i].pos,
+			desc->cylinders[i].ang, &desc->ray, desc->dist_min, closest, &d}, &is_bot))
 		{
 			best_idx = i;
 			closest = d;
-			is_bot = 0;
 		}
-		if (ray_intersect_cylinder_cap(desc->cylinders + i, bot,
-									(t_v3){-desc->cylinders[i].ang.x, -desc->cylinders[i].ang.y, -desc->cylinders[i].ang.z}, &desc->ray, desc->dist_min, closest, &d))
-		{
-			best_idx = i;
-			closest = d;
-			is_bot = 1;
-		}
-		i++;
 	}
-
-	if (best_idx == ~(t_u32)0)
-		return ;
-
-	desc->hit->dist = closest;
-	desc->hit->col = desc->cylinders[best_idx].col;
-
-	v3_muladds(&desc->ray.pos, &desc->ray.dir, closest, &desc->hit->pos);
-
-	if ( !is_bot )
-		desc->hit->norm = desc->cylinders[best_idx].ang;
-	else
-		desc->hit->norm = (t_v3){-desc->cylinders[best_idx].ang.x,
-									-desc->cylinders[best_idx].ang.y,
-									-desc->cylinders[best_idx].ang.z};
-
-	desc->hit->front = (v3_dot(&desc->ray.dir, &desc->hit->norm) < 0.0f);
-	if (!desc->hit->front)
-		v3_scalar_mul(&desc->hit->norm, -1.0f, &desc->hit->norm);
+	fill_hit(desc, best_idx, closest, is_bot);
 }
---
+++ src/inter_cyl_cap_ctx_def.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter_cyl_cap_ctx_def.h                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: losypenk <losypenk@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 13:27:30 by losypenk          #+#    #+#             */
+/*   Updated: 2026/02/18 13:27:52 by losypenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INTER_CYL_CAP_CTX_DEF_H
+# define INTER_CYL_CAP_CTX_DEF_H
+
+# include "cylinder_def.h"
+# include "ray_def.h"
+
+struct s_inter_cyl_cap_ctx
+{
+	t_cylinder const	*cy;
+	t_v3				cap_center;
+	t_v3				cap_norm;
+	t_ray const			*r;
+	t_f32				dist_min;
+	t_f32				dist_max;
+	t_f32				*out_dist;
+};
+typedef struct s_inter_cyl_cap_ctx	t_inter_cyl_cap_ctx;
+
+#endif
